b1a93df7525515cc2d5d799d22c641a8f13638df
Robrecht Cannoodt <rcannood@gmail.com>
2019-09-10 12:17:02 +0200

    fix description

--- R/data.R
+++ R/data.R
@@ -7,6 +7,8 @@
 #'
 #' @format Data are in two dimensions.
 #' @keywords data
+#'
+#' @docType data
 "annulus"
 
 #' Chainlink toy clustering data set
@@ -15,13 +17,14 @@
 #' interlocking 3-dimensional rings. 500 data points are from one ring and 500
 #' from the other ring.
 #'
-#'
 #' @format The data are in 3 dimensions, C1, C2, and C3.
 #' @references Ultsch, A.: Clustering with SOM: U*C, In Proc. Workshop on
 #' Self-Organizing Maps, Paris, France, (2005) , pp. 75-82
 #' @source
 #' <http://www.uni-marburg.de/fb12/datenbionik/data?language_sync=1>
 #' @keywords data
+#'
+#' @docType data
 "Chainlink"
 
 
